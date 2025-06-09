62f4dc60a5c6a717c2e0be258ed54acbbfa73cf9
priyanshugusain2004/pbl <priyanshgusain1@gmail.com>
2025-06-09 22:58:18 +0530

    pass

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path, { dirname } from 'path';
+import { defineConfig, loadEnv, UserConfigExport, ConfigEnv } from 'vite';
+import { fileURLToPath } from 'url';
 
-export default defineConfig({
-  base: 'PBL', // Add this line
-  define: {
-    'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig((configEnv: ConfigEnv): UserConfigExport => {
+  const env = loadEnv(configEnv.mode, process.cwd());
+  return {
+    base: '/PBL/', // Correct base for GitHub Pages
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
     }
-  }
+  };
 });
