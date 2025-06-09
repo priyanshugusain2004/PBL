57d4721506d98d9a1c09d896d55f2e8941242c35
priyanshugusain2004/pbl <priyanshgusain1@gmail.com>
2025-06-09 22:56:33 +0530

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  base: 'PBL', // Add this line
+  define: {
+    'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
